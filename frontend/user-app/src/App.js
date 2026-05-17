dc637fde9eb2be98c35073aa3750613d9a002927
Fidel Maulto <fidel.maluto77@gmail.com>
2026-05-17 22:40:51 +0100

    fix: Corrigindo o erro que retornava atravez da tradução do chrome...

--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -1,32 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
 
-  const [user, setUser] = useState([]);
-  
+  const [user, setUsers] = useState([]);
+
   const getUser = () => {
     fetch("/api/user")
-    .then(res => res.json())
-    .then(json => setUser(json))
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        setUsers(json)
+      });
   }
 
   useEffect(() => {
     getUser();
-  },[]);
-  
+  }, []);
+
   return (
     <div>
-      {
-        user.map((data) => {
-          return <>
-          <div style={{border: "1px solid gray", width: "500px"}}>
-          <h1>Name: {data.name}</h1>
-          <h1>Username: {data.username}</h1>
-          <h1>E-Mail: {data.email}</h1>
+      {user.map((data) => {
+        return <>
+          <div style={{ border: "1px solid gray", width: "500px" }}>
+            <h1>Name: {data.nome}</h1>
+            <h1>Username: {data["nome de usuário"]}</h1>
+            <h1>E-Mail: {data["e-mail"]}</h1>
           </div>
-          </>
-        })
-      }
+        </>
+      })}
     </div>
   )
 }
