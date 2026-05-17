d7ce35c05176709e4321469c29e55b39b691b513
Fidel Maulto <fidel.maluto77@gmail.com>
2026-05-17 22:51:29 +0100

    refactor: Melhorando a lógica de tradução, para os dois idiomas. E finalizando a aula

--- frontend/user-app/src/App.js
+++ frontend/user-app/src/App.js
@@ -20,13 +20,13 @@ const App = () => {
   return (
     <div>
       {user.map((data) => {
-        return <>
-          <div style={{ border: "1px solid gray", width: "500px" }}>
-            <h1>Name: {data.nome}</h1>
-            <h1>Username: {data["nome de usuário"]}</h1>
-            <h1>E-Mail: {data["e-mail"]}</h1>
+        return (
+          <div key={data.id} style={{ border: "1px solid gray", width: "500px" }}>
+            <h1>Name: {data.nome || data.name}</h1>
+            <h1>Username: {data["nome de usuário"] || data.username}</h1>
+            <h1>E-Mail: {data["e-mail"] || data.email}</h1>
           </div>
-        </>
+        )
       })}
     </div>
   )
